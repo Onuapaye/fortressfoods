347ac2b8569cf5e4057b7428ddb0a68d331013cc
Bismark Atta FRIMPONG <onuapaye@yahoo.com>
2020-06-17 15:46:24 +0200

    secret file and map marker added

---
+++ js/secret.js
@@ -0,0 +1,16 @@
+class Secret {
+    constructor(){
+        // this.key = {}
+    }
+
+    _ApiKeys = {
+        googleMAP: "AIzaSyACfyPWJyg6g5ha1rtxGCYwMTz6D_UpPCs",
+        otherAPI: "yet to provide",
+    }
+    get apiKey() {
+        return this._ApiKeys
+    }
+    set apiKey(value) {
+        this._ApiKeys = value
+    }
+}
